73e30e374e65de51dfabc6f2d3a8278329fe4a1e
Agustin Rodriguez <pachecoagus91@gmail.com>
2025-08-01 18:35:55 -0300

    Agregando archivos iniciales del proyecto

---
+++ src/main.js
@@ -0,0 +1,233 @@
+import "./style.css";
+
+//1. inializar el canvas
+const canvas = document.querySelector("canvas");
+
+const context = canvas.getContext("2d");
+
+const BLOCK_SIZE = 20;
+const BLOCK_WIDTH = 14;
+const BLOCK_HEIGHT = 30;
+
+let score = 0;
+
+canvas.width = BLOCK_SIZE * BLOCK_WIDTH;
+canvas.height = BLOCK_SIZE * BLOCK_HEIGHT;
+
+context.scale(BLOCK_SIZE, BLOCK_SIZE); // esto es lo correcto
+
+//3.board
+const board = [
+  /* ... muchas filas con 0 ... */
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1], // última fila
+];
+
+const piece = {
+  position: { x: 5, y: 5 },
+  shape: [
+    [1, 1],
+    [1, 1],
+  ],
+};
+
+//9. randon piece
+
+const PIECE = [
+  [
+    [1, 1],
+    [1, 1],
+  ],
+  [[1, 1, 1, 1]],
+  [
+    [0, 1, 0],
+    [1, 1, 1],
+  ],
+  [
+    [1, 1, 0],
+    [0, 1, 1],
+  ],
+  [
+    [1, 0],
+    [1, 0],
+    [1, 1],
+  ],
+];
+
+//2. game loop
+
+//function update() {
+//  draw();
+//  window.requestAnimationFrame(update);}
+
+//8.auto drop
+
+let dropCounter = 0;
+let lastTime = 0;
+
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+
+  if (dropCounter > 1000) {
+    piece.position.y++;
+    dropCounter = 0;
+
+    if (checkColicion()) {
+      piece.position.y--;
+      solidifyPiece();
+      removeRows();
+    }
+  }
+
+  draw();
+  window.requestAnimationFrame(update);
+}
+
+function draw() {
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  board.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        context.fillStyle = "yellow";
+        context.fillRect(x, y, 1, 1);
+      }
+    });
+  });
+
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value) {
+        context.fillStyle = "red";
+        context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1);
+      }
+    });
+  });
+
+  document.querySelector("span").innerText = score;
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    piece.position.x--;
+    if (checkColicion()) {
+      piece.position.x++;
+    }
+  }
+  if (event.key === "ArrowRight") {
+    piece.position.x++;
+    if (checkColicion()) {
+      piece.position.x--;
+    }
+  }
+  if (event.key === "ArrowDown") {
+    piece.position.y++;
+    if (checkColicion()) {
+      piece.position.y--;
+      solidifyPiece();
+      removeRows();
+    }
+  }
+
+  if (event.key === "ArrowUp") {
+    const rotated = [];
+
+    for (let i = 0; i < piece.shape[0].length; i++) {
+      const row = [];
+      for (let j = piece.shape.length - 1; j >= 0; j--) {
+        row.push(piece.shape[j][i]);
+      }
+      rotated.push(row);
+    }
+    const previousShape = piece.shape;
+    piece.shape = rotated;
+    if (checkColicion()) {
+      piece.shape = previousShape;
+    }
+  }
+});
+
+function checkColicion() {
+  return piece.shape.find((row, y) => {
+    return row.find((value, x) => {
+      return (
+        value !== 0 && board[y + piece.position.y]?.[x + piece.position.x] !== 0
+      );
+    });
+  });
+}
+
+function solidifyPiece() {
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        board[y + piece.position.y][x + piece.position.x] = 1;
+      }
+    });
+  });
+
+  //reset posision
+  piece.position.x = Math.floor(BLOCK_WIDTH / 2 - 2);
+  piece.position.y = 0;
+
+  //get randon piece
+  piece.shape = PIECE[Math.floor(Math.random() * PIECE.length)];
+  //game over
+  if (checkColicion()) {
+    window.alert("game over !!!");
+    board.forEach((row) => row.fill(0));
+  }
+}
+
+function removeRows() {
+  const rowsToRemove = [];
+
+  board.forEach((row, y) => {
+    if (row.every((value) => value === 1)) {
+      rowsToRemove.push(y);
+    }
+  });
+
+  rowsToRemove.forEach((y) => {
+    board.splice(y, 1);
+    const newRow = Array(BLOCK_WIDTH).fill(0);
+    board.unshift(newRow);
+    score += 10;
+  });
+}
+
+update();
+
+//agustin Rodriguez
